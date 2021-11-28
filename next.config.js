ee2bbcee7e9313c316689e79ec45075ede2ab0e9
quavious <leenowon97@gmail.com>
2021-11-29 02:12:57 +0900

    Apply PWA

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
   reactStrictMode: true,
-  images: {
-    domains: ['*'],
-  },
-};
+});
